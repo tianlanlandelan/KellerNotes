10f86f7ee998b90058bacbc8cd790b4f19fe948f
yangkaile <guyexing@foxmail.com>
2020-03-05 12:11:58 +0800

    注册功能完整代码

---
+++ 12.注册2/web/src/views/Login.vue
@@ -0,0 +1,26 @@
+<!-- 登录页面 -->
+<template>
+    <div>
+        Login
+    </div>
+</template>
+
+<script>
+    export default{
+		data(){
+			return{
+				
+			}
+		},
+		methods:{
+			
+		},mounted(){
+			
+		}
+	}
+</script>
+
+<style scoped>
+</style>
+
+
